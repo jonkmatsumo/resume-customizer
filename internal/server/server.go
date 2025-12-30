dd913192a9c40c0ac1f728091cccf1cb3bc6d76e
jonkmatsumo <jonkmatsumo@gmail.com>
2025-12-30 15:08:50 -0800

    add PUT and DELETE to CORS

--- internal/server/server.go
+++ internal/server/server.go
@@ -222,8 +222,9 @@ func (s *Server) Start() error {
 func (s *Server) withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
